90581889fe900e63da8c7a93fe6c468235ebc374
miguel <miguel.iglesias@gmail.com>
2019-10-30 23:15:53 +0100

    Test1

--- mapa_barocci_files/async.js
+++ mapa_barocci_files/async.js
@@ -1,3 +1,4 @@
+alert("1")
 !function(a, b) {
     "object" == typeof exports && "undefined" != typeof module ? module.exports = b() : "function" == typeof define && define.amd ? define(b) : a.moment = b();
 }(this, function() {
@@ -10012,7 +10013,7 @@ $.widget("nh.floorMap", {
                     roomClass += "room-available";
                     roomContainer.off("click touchend");
                     if (this.options.zoomType === "In") {
-                        roomContainer.on("click touchend", this.selectRoom.bind(this));
+                        //roomContainer.on("click touchend", this.selectRoom.bind(this));
                     }
                 } else {
                     roomClass += "room-noavailable";
