b384a4e61552ca40e2945c86be568db9ecd5ff32
TG399GZ <marc.casanova.coma@es.ey.com>
2019-12-19 15:40:13 +0100

    v7.2

--- appmobile/async.js
+++ appmobile/async.js
@@ -41110,7 +41110,7 @@ var Landings = (function(){
 						selectLink.href.lastIndexOf("")
 					);
           selectLink.href = formatLink + sublink;
-          selectLink.target("");
+          selectLink.target = "";
           selectLink.click();
 				}
         if(isMobile.iOS()){
