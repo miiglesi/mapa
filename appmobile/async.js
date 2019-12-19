da1dd8244638c35cd83c9e770410598af5348068
TG399GZ <marc.casanova.coma@es.ey.com>
2019-12-19 16:37:06 +0100

    v7.5

--- appmobile/async.js
+++ appmobile/async.js
@@ -41110,7 +41110,7 @@ var Landings = (function(){
 						selectLink.href.lastIndexOf("")
 					);
           //selectLink.href = formatLink + sublink;
-          selectLink.target = "_self";
+          selectLink.target = "";
           selectLink.href = "market://details?id=com.mo2o.nh";
           selectLink.click();
 				}
